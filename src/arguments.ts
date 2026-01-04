15887dfb0d99a0d07bb106699a0459dffc6eba6b
barronbytes <jonathan.m.barron@gmail.com>
2026-01-04 18:50:44 -0500

    Added getCmdAndArgs function

--- src/arguments.ts
+++ src/arguments.ts
@@ -1,7 +1,37 @@
+import type { CommandRegistry } from "./commands.types.js";
+
+
 /* 
-* Returns list of arguments passed in terminal
+* Returns a list of arguments passed in the terminal.
+* Exits with code 1 if no user arguments are provided.
 */
 export function getArguments(): string[] {
-    const args = process.argv;
-    return args.slice(2);
+    const args = process.argv.slice(2); // slice first 2 user args → skip Node + script path
+
+    if (args.length < 2) {
+        console.error("Error: Must provide command name and at least one argument.");
+        console.log("Usage: npm run start -- commandName ...arguments");
+        process.exit(1);
+    }
+
+    return args;
+}
+
+
+/*
+* Returns command name and argument from CLI arguments.
+* Exits with code 1 if both not provided.
+*/
+export function getCmdAndArgs(registry: CommandRegistry, args: string[]): [string, string[]] {
+    const cmdName = args[0];
+    const cmdArgs = args.slice(1);
+
+    // Check that the command exists in the registry
+    if (!registry[cmdName]) {
+        console.error(`Error: Unknown command "${cmdName}".`);
+        console.log("Available commands:", Object.keys(registry).join(", "));
+        process.exit(1);
+    }
+
+    return [cmdName, cmdArgs];
 }
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
-import { getArguments } from "./arguments.js";
-import { updateUsername, readConfig } from "./file-handling.js";
-import type { CommandRegistry, CommandHandler } from "./commands.types.js";
+import { getArguments, getCmdAndArgs } from "./arguments.js";
+import { readConfig } from "./file-handling.js";
+import type { CommandRegistry } from "./commands.types.js";
 import * as Cmds from "./commands.js";
 
 
 function main() {
-    const args = getArguments();
-    console.log(`Arguments: ${args}`);
-
+    // Register command
     const registry: CommandRegistry = {}
     Cmds.registerCommand(registry, "login", Cmds.handlerLogin);
+
+    // Get CLI command name and arguments
+    const args = getArguments();
+    const [cmdName, cmdArgs] = getCmdAndArgs(registry, args);
+
+    // Update username on JSON file
+    Cmds.runCommand(registry, cmdName, ...cmdArgs);
 
     // Read and print the JSON file
     const currentConfig = readConfig();
