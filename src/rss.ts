eec445e1e82109fd3f242394fc8b10ff7483147f
barronbytes <jonathan.m.barron@gmail.com>
2026-01-11 15:59:31 -0500

    Normalized fetch response

--- src/rss.ts
+++ src/rss.ts
@@ -3,26 +3,51 @@ import * as RssHttp from "./rss.http.js";
 import * as RssZod from "./rss.types.js";
 
 
+/* Fetches an RSS feed from the given URL. */
 export async function fetchFeed(requestURL: string): Promise<RssZod.RSSFeed> {
+
     try {
+        // Fetch RSS feed
         const response = await fetch(requestURL, RssHttp.settingsGET);
         if (!response.ok) throw new Error(`HTTP Error: status code: ${response.status}.`);
 
         const xmlString = await response.text();
-        if (!xmlString.trim()) throw new Error(`HTTP Error: empty XML response.`);
+        if (!xmlString.trim()) throw new Error(`Fetch Error: empty XML response.`);
 
+        // Parse and validate fetch response
         const parsedXML = RssHttp.parser.parse(xmlString);
         const validatedXML = RssZod.RSSFeedSchema.parse(parsedXML);
 
-        return validatedXML;
+        // Ensure channel.item is always an array
+        let itemsArray: RssZod.RSSItem[] = [];              // defaults to empty array
+        if (validatedXML.channel.item) {
+            if (Array.isArray(validatedXML.channel.item)) { // already array => store as array
+                itemsArray = validatedXML.channel.item;
+            } else {                                        // single item => store as array
+                itemsArray = [validatedXML.channel.item];
+            }
+        }
+
+        // Create feed with normalized items array
+        const normalizedXML: RssZod.RSSFeed = {
+            ...validatedXML,                // shallow copy
+            channel: {
+                ...validatedXML.channel,    // shallow copy
+                item: itemsArray,           // overwrite validatedXML.channel.item
+            },
+        }
+
+        // Return feed with normalized items array
+        return normalizedXML;
     }
+
     catch (error) {
         if (error instanceof z.ZodError) {
-            throw new Error(`Validation failure: ${JSON.stringify(error.issues)}`);
+            throw new Error(`Validation Error: ${JSON.stringify(error.issues)}`);
         } else if (error instanceof Error) {
-            throw new Error(`Fetch/HTTP failure: ${error.message}`);
+            throw new Error(error.message);
         } else {
-            throw new Error(`Unexpected error: ${String(error)}`);
+            throw new Error(`Unexpected Error: ${String(error)}`);
         }
     }
 }
